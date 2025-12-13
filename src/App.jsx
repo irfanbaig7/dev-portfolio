0de514ac00e2d6ab41d30563ce1236b37146c248
irfan baig <ibaig9356@gmail.com>
2025-12-13 20:42:00 +0530

    chore: set up global styles and base app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Navbar from "../src/components/Navbar.jsx"
+import Home from "../src/sections/Home.jsx"
+import About from "../src/sections/About.jsx"
+import Skills from "../src/sections/Skills.jsx"
+import Projects from "../src/sections/Projects.jsx"
+import Expreiense from "../src/sections/Experience.jsx"
+import Testimonials from "../src/sections/Testimonials.jsx"
+import Contact from "../src/sections/Contact.jsx"
+import Footer from "../src/sections/Footer.jsx"
+
 
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+    <div className='relative gradient text-white'>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Expreiense />
+      <Testimonials />
+      <Contact />
+      <Footer />    
+      
+    </div>
   )
 }
 
